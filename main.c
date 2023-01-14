ffbb5d3d73a914b01ad0c8f7664f45422ad585e4
Jvaljer <abbel.haol@gmail.com>
2023-01-14 16:02:13 +0100

    casual commit

--- main.c
+++ main.c
@@ -7,9 +7,9 @@ int main(int argc, char* argv){
     /*
      * Test Section
      */
-    TestCoord(); //passed succesfully 
-    TestSlot(); //passed succesfully 
-    TestToken(); //passed succesfully 
+    //TestCoord();  
+    //TestSlot(); 
+    //TestToken(); 
     TestBoard(); 
     /*
      * Core section
