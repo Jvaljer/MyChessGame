373500c9cc58a459609cf037be7649ea2069f2be
Jvaljer <abbel.haol@gmail.com>
2023-01-14 16:01:46 +0100

    Board has been redone successfully (gonna add printing & moving function

--- Board.c
+++ Board.c
@@ -1,99 +1,48 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include "Board.h"
 
 Board* new_Board(){
-    int size = 8;
+    Board* B = malloc(sizeof(Board));
     Role line[8] = { ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK };
-    
-    Board* B;
-    for(int i=0; i<size; i++){
-        for(int j=0; j<size; j++){
-            Slot* S_ = new_Slot(new_Coord(i,j));
-            B->grid[i][j] = S_;
-            if(i=0){
-                Token* T_ = new_Token(S_,line[j],WHITE);
-                B->hands[0][size+j] = T_;
-               S_->occupied = 1;
-               T_->onBoard = 1;
-            } else if(i=1){
-                Token* T_ = new_Token(S_,PAWN,WHITE);
-                B->hands[0][j] = T_;
-                S_->occupied = 1;
-                T_->onBoard = 1;
-            } else if(i=6){
-                Token* T_ = new_Token(S_,PAWN,BLACK);
-                B->hands[1][j] = T_;
-                S_->occupied = 1;
-                T_->onBoard = 1;
-            } else if(i=7){
-                Token* T_ = new_Token(S_,line[j],BLACK);
-                B->hands[1][size+j] = T_;
-                S_->occupied = 1;
-                T_->onBoard = 1;
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            Coord* C_ = new_Coord(i,j);
+            Slot* S_ = new_Slot(C_);
+            B->grid[i][j] = S_ ;
+            //assert( EqSlotP( B->grid[i][j], S_ ));
+            if(i==0){
+                Token* T_ = new_Token(B->grid[i][j],line[j],WHITE);
+                B->hands[0][8+j] = T_ ;
+                //assert( EqTokenP(T_,B->hands[0][8+j]) );
+            } else if(i==1){
+                Token* T_ = new_Token(B->grid[i][j],PAWN,WHITE);
+                B->hands[0][j] = T_ ;
+                //assert( EqTokenP(T_,B->hands[0][j]) );
+            } else if(i==6){
+                Token* T_ = new_Token(B->grid[i][j],PAWN,BLACK);
+                B->hands[1][j] = T_ ;
+                //assert( EqTokenP(T_,B->hands[1][j]) );
+            } else if(i==7){
+                Token* T_ = new_Token(B->grid[i][j],line[j],BLACK);
+                B->hands[1][8+j] = T_ ;
+                //assert( EqTokenP(T_,B->hands[1][8+j]) );
             }
-            //assert( EqSlotP(B->S[i][j],S_) );
         }
     }
     return B;
 }
 
-Slot* GetSlot(Board* B, Coord* C){
-    return B->grid[C->x][C->y];
-}
-
-Token* GetToken(Board* B, Slot* S){
-    int size = 8;
-    for(int i=0; i<2; i++){
-        for(int j=0; j<size; j++){
-            if(EqSlotP(B->hands[i][j]->slot,S) && S->occupied){
-                return B->hands[i][j];
-            }
-        }
-    }
-}
-
-void PrintBoard(Board* B){
-    int size = 8;
-    char* transition_line = "-----------------------------------------";
-    printf("  a    b    c    d    e    f    g    h\n");
-    printf("%s\n", transition_line);
-    for(int i=0; i<size; i++){
-        for(int j=0; j<size; j++){
-            if(j==0){
-                printf("|");
-            }
-            if( B->grid[i][j]->occupied==1){
-                Token* T_ = GetToken(B,B->grid[i][j]);
-                printf(" %s ",(char*)T_->id);
-            } else {
-                printf("    ");
-            }
-            printf("|");
-        }
-        printf(" %d\n", i);
-        printf("%s\n",transition_line);
-    }
-
-}
-
 void TestBoard(){
     printf("#- BOARD TEST START\n");
-    printf("Testing Board Constructor -> ");
+    printf("Testing Constructor : ");
     Board* B = new_Board();
-    Slot* S00 = new_Slot(new_Coord(0,0));
-    Slot* S57 = new_Slot(new_Coord(5,7));
-    assert( EqSlot(B->grid[0][0], S00)==1 );
-    assert( EqSlot(B->grid[5][7], S57)==1 );
-    assert( EqSlotP(B->grid[5][7], S57)==0 );
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            assert( B->grid[i][j]->coord->x==i && B->grid[i][j]->coord->y==j);
+        }
+    }
     printf(" check\n");
-
-    printf("Testing Slot Getter -> ");
-    Slot* S35 = GetSlot(B,new_Coord(3,5));
-    assert( EqSlot(B->grid[3][5], S35) );
-    assert( EqSlotP(B->grid[3][5], S35) );
-    printf(" check\n");
-    
     printf(" -> BOARD TEST END -# \n\n");
 }
--- Board.h
+++ Board.h
@@ -2,64 +2,22 @@
 #define BOARD_H
 
 #include "Token.h"
+
 /*
- * Board is simply defined by a grid of Slots (pointer on Slots)
- *  and the Hands (pointer on Tokens)
+ * Board is defined by 2 lists of Slots & Tokens which 
+ *  represents the physical board & all tokens placed on the board 
  */
 typedef struct Board{
-    Slot* grid [8][8];
-    Token* hands[2][16];
+    Slot *grid[8][8];
+    Token *hands[2][16];
 }Board;
 
 /*
- * Board Constructor 
- * args : None 
- * result : a 8x8 board with 'grid' full of Slots 
- *      which has the same coords as there position
- *         + all pieces present in 'hands'
+ * Board Constructor
+ *  Creatign the 2 lists 
+ *      Slots has Coords of the same indexs
  */
 Board* new_Board();
-
-/*
- * Getter for a precise slot 
- * args : (B:Board*) (C:Coord*)
- * result : the grid's slot which coordinates are C
- */
-Slot* GetSlot(Board* B, Coord* C);
-
-/*
- * Getter for a precise token 
- * args : (B:Board*) (S:Slot*)
- * result : the hand's token which is on S
- */
-Token* GetToken(Board* B, Slot* S);
-
-/*
- * Board Printing 
- * args : (B:Board*)
- * result : brand new board -> 
- *        a    b    c    d    e    f    g    h
- *      -----------------------------------------
- *      | bR | bK | bB | bQ | BK | bB | bK | bR |  1
- *      -----------------------------------------  
- *      | bP | bP | bP | bP | bP | bP | bP | bP |  2
- *      ----------------------------------------- 
- *      |    |    |    |    |    |    |    |    |  3
- *      -----------------------------------------
- *      |    |    |    |    |    |    |    |    |  4
- *      -----------------------------------------  
- *      |    |    |    |    |    |    |    |    |  5
- *      -----------------------------------------
- *      |    |    |    |    |    |    |    |    |  6
- *      -----------------------------------------
- *      | wP | wP | wP | wP | wP | wP | wP | wP |  7
- *      -----------------------------------------
- *      | wR | wK | wB | wQ | WK | wB | wK | wR |  8
- *      -----------------------------------------
- * token's name & position are printed by recognizing where they are 
- *  not only print the previous grid...
- */
-void PrintBoard(Board* B);
 
 /*
  * Test Function for Board stuff
@@ -68,4 +26,5 @@ void PrintBoard(Board* B);
  *          error if an assertion false
  */
 void TestBoard();
+
 #endif
